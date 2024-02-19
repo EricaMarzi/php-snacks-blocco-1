3c82a4b2d855a8c61d5a9f6dddaedce39baed2de
EricaMarzi <erica.marzi.1993@gmail.com>
2024-02-19 15:28:01 +0100

    authenticator name

--- Snack2/script.php
+++ Snack2/script.php
@@ -1,6 +1,20 @@
 <!--Con un form passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”-->
 
-<?php ?>
+<?php 
+
+$name = $_GET['name'] ?? '';
+$age = $_GET['age'] ?? '';
+$email = $_GET['email'] ?? '';
+
+if(strlen($name)< 3) {
+    $message = 'Accesso Negato';
+    $class = 'text-danger';
+}else {
+    $message = 'Accesso Riuscito';
+    $class = 'text-success';
+};
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -8,8 +22,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>snack2</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    
+    <h3 class="<?= $class ?>"><?= $message ?></h3>
 </body>
 </html>
